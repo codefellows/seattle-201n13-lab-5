68fb2e568ef455055eb217a29027fd1f81f72774
Wyatt Wedell <wyattearp24@aol.com>
2019-02-13 13:33:26 -0800

    making of my 3rd branch

--- app.js
+++ app.js
@@ -70,7 +70,7 @@ Test this function by hand in the console to get it working, and when you think 
 }
 
 // Here is the test for sumAndMultiply(); uncomment it to run it
-testSumAndMultiply(4,7,5);
+// testSumAndMultiply(4,7,5);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
